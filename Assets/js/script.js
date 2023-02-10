8afeda2be7361f55f4f68a439f6a1e35de42b9ea
LENTIO SECHOU <lentiodev@gmail.com>
2023-02-10 15:05:18 +0000

    lentio push

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,68 +1,53 @@
-function GetInfo(){
-	const restaurantName= document.getElementById('restaurantName');
+const btn = document.querySelector('#searchBtn')
+
+btn.addEventListener('click', () => {
+    console.log('zoom in')
+})
+
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement(
+        {pageLanguage: 'en' },
+        'google_translate_element'
+    );
 }
 
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '43a48491b6mshe887897b8e58e78p1e0934jsn82cdea6f0aaa',
-		'X-RapidAPI-Host': 'tripadvisor16.p.rapidapi.com'
-	}
-};
+var cities = [
+    { name: "London", lat: 51.5074, lng: -0.1278 },
+    { name: "Manchester", lat: 53.4808, lng: -2.2426 },
+    { name: "Birmingham", lat: 52.4862, lng: -1.8904 },
+    { name: "Liverpool", lat: 53.4308, lng: -2.9608 },
+    { name: "Bristol", lat: 51.4545, lng: -2.5879 }
+  ];
 
-fetch('https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchRestaurants?locationId=304554', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(error => console.error(error));
+  var map = L.map('mapid').setView([52.3555, -1.1743], 5);
 
-console.log(fetch())
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
 
+  var markers = [];
+  cities.forEach(function(city) {
+    var marker = L.marker([city.lat, city.lng]).addTo(map);
+    marker.bindPopup(city.name);
+    markers.push(marker);
+  });
 
-function GetInfo(){
-	const newTranslation= document.getElementById('newTranslation');
-}
+  document.getElementById("searchBtn").addEventListener("click", function() {
+    var cityName = document.getElementById("citySearch").value;
+    markers.forEach(function(marker) {
+      if (marker._popup._content.toLowerCase() === cityName.toLowerCase()) {
+        marker.openPopup();
+      } else {
+        marker.closePopup();
+      }
+    });
+  });
 
-const encodedParams = new URLSearchParams();
-encodedParams.append("q", "Hello, world!");
-encodedParams.append("target", "es");
-encodedParams.append("source", "en");
+  const unsplashApiKey = "I2CEN80WyrKtGF99TtTTe4bmuO-I85cJDYQyZvKZFt4"
+  const queryKey = 'https://api.unsplash.com/photos/?client_id=' + unsplashApiKey
 
-const options = {
-	method: 'POST',
-	headers: {
-		'content-type': 'application/x-www-form-urlencoded',
-		'Accept-Encoding': 'application/gzip',
-		'X-RapidAPI-Key': 'a16aec5effmsh1f03abc2bac9cfap1d6c69jsn50e18bdb7471',
-		'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
-	},
-	body: encodedParams
-};
-
-fetch('https://google-translate1.p.rapidapi.com/language/translate/v2', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-
-console.log(fetch(url))
-
-
-function GetInfo(){
-	const newCity = document.getElementById('newCity');
-	const newRestaurant = document.getElementById('newRestaurant');
-	const newLocalEntertainment= document.getElementById('newLocalEntertainment');
-}
-
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'a16aec5effmsh1f03abc2bac9cfap1d6c69jsn50e18bdb7471',
-		'X-RapidAPI-Host': 'maptiles.p.rapidapi.com'
-	}
-};
-
-fetch('https://maptiles.p.rapidapi.com/en/map/v1/3/6/3.png', options)
+fetch(queryKey)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
 
-console.log(fetch(url))
