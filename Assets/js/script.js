9d269d6ba8fa829916fde7ad115aafee3068c3ab
Axel Jamal <axeltaj@gmail.com>
2023-02-13 17:58:25 +0000

    push changes from lentio; design updates

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,164 +1,98 @@
-const btn = document.querySelector('#searchBtn')
+//unsplash api
+const searchInput = document.getElementById("searchTerm");
+const searchBtn = document.getElementById("searchButton");
+const imagesContainer = document.getElementById("images-container");
 
-btn.addEventListener('click', () => {
-    console.log('zoom in')
-})
+// Function to fetch photos from the Unsplash API based on a search term
+// add local store for the images
 
-function googleTranslateElementInit() {
-    new google.translate.TranslateElement(
-        {pageLanguage: 'en' },
-        'google_translate_element'
-    );
-}
+const fetchPhotos = (searchTerm) => {
+  fetch(
+    `https://api.unsplash.com/search/photos?client_id=DUM8TbIYndcAruIhX49IMY-89SQd2zLXmzsfrlfxsBo&query=${searchTerm}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      // Clear the images container
+      imagesContainer.innerHTML = "";
+      // Loop through the photos and add them to the HTML
+      data.results.forEach((photo) => {
+        const div = document.createElement("div");
+        div.classList.add("card", "m-2");
+        div.style.width = "18rem";
+        const img = document.createElement("img");
+        img.src = photo.urls.regular;
+        img.classList.add("card-img-top");
+        div.appendChild(img);
+        imagesContainer.appendChild(div);
+      });
+    });
+};
 
-var cities = [
-    { name: "London", lat: 51.5074, lng: -0.1278 },
-    { name: "Manchester", lat: 53.4808, lng: -2.2426 },
-    { name: "Birmingham", lat: 52.4862, lng: -1.8904 },
-    { name: "Liverpool", lat: 53.4308, lng: -2.9608 },
-    { name: "Bristol", lat: 51.4545, lng: -2.5879 }
-  ];
+// Fetch photos when the page loads
+fetchPhotos("");
 
-  var map = L.map('mapid').setView([52.3555, -1.1743], 5);
+// Listen for the search button to be clicked
+searchBtn.addEventListener("click", () => {
+  const searchTerm = searchInput.value;
+  fetchPhotos(searchTerm);
+});
 
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map);
+// openweather api
 
-  var markers = [];
-  cities.forEach(function(city) {
-    var marker = L.marker([city.lat, city.lng]).addTo(map);
-    marker.bindPopup(city.name);
-    markers.push(marker);
-  });
+const apiKey = "bf448c820b7abc0e9f0744329593ee18";
+const forecastContainer = document.getElementById("forecast-container");
+const searchButton = document.getElementById("searchButton");
 
-  document.getElementById("searchBtn").addEventListener("click", function() {
-    var cityName = document.getElementById("citySearch").value;
-    markers.forEach(function(marker) {
-      if (marker._popup._content.toLowerCase() === cityName.toLowerCase()) {
-        marker.openPopup();
-      } else {
-        marker.closePopup();
+searchButton.addEventListener("click", () => {
+  const cityInput = document.getElementById("searchTerm").value;
+  fetch(
+    `http://api.openweathermap.org/data/2.5/forecast?q=${cityInput}&appid=${apiKey}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      forecastContainer.innerHTML = "";
+      for (let i = 0; i < data.list.length; i += 8) {
+        const temperature = data.list[i].main.temp - 273.15;
+        const iconCode = data.list[i].weather[0].icon;
+        const iconUrl = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
+        forecastContainer.innerHTML += `
+                <div>
+                  <h3>${new Date(
+                    data.list[i].dt * 1000
+                  ).toLocaleDateString()}</h3>
+                  <p>Temperature: ${temperature.toFixed(2)}°C</p>
+                  <img src="${iconUrl}" alt="Weather Icon">
+                  <p>Description: ${data.list[i].weather[0].description}</p>
+                </div>
+              `;
       }
     });
-  });
+});
 
-  const unsplashApiKey = "I2CEN80WyrKtGF99TtTTe4bmuO-I85cJDYQyZvKZFt4"
-  const queryKey = 'https://api.unsplash.com/photos/?client_id=' + unsplashApiKey
+// map api
 
-fetch(queryKey)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+const citySearch = document.getElementById("searchTerm");
+const searchMap = document.getElementById("searchButton");
+const map = L.map("map").setView([51.505, -0.09], 13);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
 
-  // SIGN BUTTON AND UNSPLASH API 
-
-  const signupButton = document.getElementById("signup-button");
-  const signupModal = document.getElementById("signup-modal");
-  const signupForm = document.getElementById("signup-form");
-  const signinButton = document.getElementById("signin-button");
-  const signinModal = document.getElementById("signin-modal");
-  const signinForm = document.getElementById("signin-form");
-  const closeButton = document.querySelector(".close");
-
-  signupButton.addEventListener("click", function () {
-    signupModal.style.display = "block";
-  });
-
-  signinButton.addEventListener("click", function () {
-    signinModal.style.display = "block";
-  });
-
-  closeButton.addEventListener("click", function () {
-    signupModal.style.display = "none";
-    signinModal.style.display = "none";
-  });
-
-  signupForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    const username = document.getElementById("username").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-
-    console.log("Username:", username);
-    console.log("Email:", email);
-    console.log("Password:", password);
-
-    // Store the user information in local storage
-    localStorage.setItem("username", username);
-    localStorage.setItem("email", email);
-    localStorage.setItem("password", password);
-
-    signupModal.style.display = "none";
-  });
-
-  signinForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    const signinUsername = document.getElementById("signin-username").value;
-    const signinPassword = document.getElementById("signin-password").value;
-
-    console.log("Signin username:", signinUsername);
-    console.log("Signin password:", signinPassword);
-
-    // Get the user information from local storage
-    const username = localStorage.getItem("username");
-    const password = localStorage.getItem("password");
-
-    // Check if the sign in information matches the information stored in local storage
-    if (signinUsername === username && signinPassword === password) {
-      console.log("Sign in successful!");
-      signinModal.style.display = "none";
-
-      // Give the user full access to the HTML file
-      document.body.style.pointerEvents = "auto";
-    } else {
-      console.log("Sign in failed. Please try again.");
-    }
-  });
-
-  // unsplash api
-
-  const searchInput = document.getElementById("search-input");
-  const searchButton = document.getElementById("search-button");
-  const imagesContainer = document.getElementById("images-container");
-
-  // Function to fetch photos from the Unsplash API based on a search term
-  // add local store for the images
-
-  const fetchPhotos = (searchTerm) => {
-    fetch(
-      `https://api.unsplash.com/search/photos?client_id=I2CEN80WyrKtGF99TtTTe4bmuO-I85cJDYQyZvKZFt4&query=${searchTerm}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        // Clear the images container
-        imagesContainer.innerHTML = "";
-        // Loop through the photos and add them to the HTML
-        data.results.forEach((photo) => {
-          const div = document.createElement("div");
-          div.classList.add("card", "m-2");
-          div.style.width = "18rem";
-          const img = document.createElement("img");
-          img.src = photo.urls.regular;
-          img.classList.add("card-img-top");
-          div.appendChild(img);
-          imagesContainer.appendChild(div);
-        });
-      });
-  };
-
-  // Fetch photos when the page loads
-  fetchPhotos("");
-
-  // Listen for the search button to be clicked
-  searchButton.addEventListener("click", () => {
-    const searchTerm = searchInput.value;
-    fetchPhotos(searchTerm);
-  });
-
-  // unsplash api
-
-  
-
+searchMap.addEventListener("click", () => {
+  const city = citySearch.value;
+  const API_KEY = "c7a34fd37a9e46e89b140f5d67bf1b54";
+  const API_URL = `https://api.opencagedata.com/geocode/v1/json?q=${city}&key=${API_KEY}`;
+  axios
+    .get(API_URL)
+    .then((response) => {
+      const lat = response.data.results[0].geometry.lat;
+      const lng = response.data.results[0].geometry.lng;
+      const latLng = [lat, lng];
+      map.setView(latLng, 13);
+      L.marker(latLng).addTo(map);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
